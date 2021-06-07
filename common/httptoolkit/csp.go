8a3c0a20db5179f19c5c0a96c3b09d10a5f78d2f
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:22:49 +0530

    [+] CSP Grab fills the csp data [+]

--- common/httptoolkit/csp.go
+++ common/httptoolkit/csp.go
@@ -1,5 +1,12 @@
 package httptoolkit
 
+import (
+	"net/http"
+	"strings"
+
+	"github.com/krishpranav/httptoolkit/common/slice"
+)
+
 var CSPHeaders = []string{
 	"Content-Security-Policy",               // standard
 	"Content-Security-Policy-Report-Only",   // standard
@@ -9,4 +16,27 @@ var CSPHeaders = []string{
 
 type CSPData struct {
 	Domains []string `json:"domains,omitempty"`
+}
+
+func (h *HTTPTOOLKIT) CSPGrab(r *http.Response) *CSPData {
+	domains := make(map[string]struct{})
+	for _, cspHeader := range CSPHeaders {
+		cspRaw := r.Header.Get(cspHeader)
+		if cspRaw != "" {
+			rules := strings.Split(cspRaw, ";")
+			for _, rule := range rules {
+				tokens := strings.Split(rule, " ")
+				for _, t := range tokens {
+					if isPotentialDomain(t) {
+						domains[t] = struct{}{}
+					}
+				}
+			}
+		}
+	}
+
+	if len(domains) > 0 {
+		return &CSPData{Domains: slice.ToSlice(domains)}
+	}
+	return nil
 }
