f6dbd8044e40a19e00ede0a44ce2322436cc4465
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:28:23 +0530

    [+] encodings [+]

--- common/httptoolkit/encodings.go
+++ common/httptoolkit/encodings.go
@@ -5,12 +5,33 @@ import (
 	"io/ioutil"
 
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
 	"golang.org/x/text/transform"
 )
 
 func Decodegbk(s []byte) ([]byte, error) {
 	I := bytes.NewReader(s)
 	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func Decodebig5(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, traditionalchinese.Big5.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func Encodebig5(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, traditionalchinese.Big5.NewEncoder())
 	d, e := ioutil.ReadAll(O)
 	if e != nil {
 		return nil, e
