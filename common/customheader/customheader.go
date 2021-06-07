951f0c99426615a4a42cab79b5723e3a28e4dd19
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:06:57 +0530

    [+] setted a new global header [+]

--- common/customheader/customheader.go
+++ common/customheader/customheader.go
@@ -5,3 +5,8 @@ type CustomHeaders []string
 func (c *CustomHeaders) String() string {
 	return "custom global headers"
 }
+
+func (c *CustomHeaders) Set(value string) error {
+	*c = append(*c, value)
+	return nil
+}
