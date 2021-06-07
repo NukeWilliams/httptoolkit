ba75a9345735e914ff565b693b8c4367e7de5506
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:27:29 +0530

    [+] decode gbk update [+]

---
+++ common/httptoolkit/encodings.go
@@ -0,0 +1,19 @@
+package httptoolkit
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+func Decodegbk(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
