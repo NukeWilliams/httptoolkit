b5ed01e37a89230bb89dd7b1859b797f7b09178c
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:09:11 +0530

    [+] func for a new global header [+]

--- common/customlist/customlist.go
+++ common/customlist/customlist.go
@@ -5,3 +5,8 @@ type CustomList []string
 func (c *CustomList) String() string {
 	return "custom global list"
 }
+
+func (c *CustomList) Set(value string) error {
+	*c = append(*c, value)
+	return nil
+}
