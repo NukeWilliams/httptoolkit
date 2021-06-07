2d0b5ab92347660d9808e2119ad1305e44ccb8c1
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:19:21 +0530

    [+] CSP Header update [+]

---
+++ common/httptoolkit/csp.go
@@ -0,0 +1,8 @@
+package httptoolkit
+
+var CSPHeaders = []string{
+	"Content-Security-Policy",               // standard
+	"Content-Security-Policy-Report-Only",   // standard
+	"X-Content-Security-Policy-Report-Only", // non - standard
+	"X-Webkit-Csp-Report-Only",              // non - standard
+}
