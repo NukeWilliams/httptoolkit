df11edacfe40064575a1aa5af8889620c6d89b47
krishpranav <krisna.pranav@gmail.com>
2021-06-07 12:30:38 +0530

    [+] filter response with string [+]

--- common/httptoolkit/filter.go
+++ common/httptoolkit/filter.go
@@ -1,9 +1,21 @@
 package httptoolkit
 
+import "strings"
+
 type Filter interface {
 	Filter(response *Response) (bool, error)
 }
 
 type FilterString struct {
 	Keywords []string
+}
+
+func (f FilterString) Filter(response *Response) (bool, error) {
+	for _, keyword := range f.Keywords {
+		if strings.Contains(response.Raw, keyword) {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
